948af35f7024e06b1f5d913a2e2ae8fdb6b26ad0
Luna <imlvnaa@gmail.com>
2024-01-30 13:04:10 -0500

    comic code ligatures ftw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 import ctp from '@catppuccin/tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Comic Code Ligatures', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	safelist: [
 		{
